fe72bde2f167ada2b8a20cecfd5f44b7e242b561
Bastian Ulke <bastian.ulke@microsoft.com>
2024-07-02 10:50:33 +0200

    limit to id.

--- appgw_list-all_graph.sh
+++ appgw_list-all_graph.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-az graph query --graph-query 'resources | where type == "microsoft.network/applicationgateways"' --query "data[].id" -o json > appgw_inventory.json
+az graph query --graph-query 'resources | where type == "microsoft.network/applicationgateways" | project id' --query "data[].id" -o json > appgw_inventory.json
 
